ee7f0e2c1cd27437eca174050bc9fab9601a34b1
jfranklin12 <j.franks725@gmail.com>
2023-06-15 18:21:53 -0700

    preview renders breaks

--- src/Preview.js
+++ src/Preview.js
@@ -2,7 +2,13 @@ import React from "react";
 import { marked } from "marked";
 
 const Preview = ({ value }) => {
-    const convertedMarkdown = marked(value);
+    const convertMarkdownToHTML = (markdown) => {
+        const renderer = new marked.Renderer();
+        renderer.breaks = true;
+
+        return marked(markdown, { renderer: renderer });
+    }
+    const convertedMarkdown = convertMarkdownToHTML(value);
 
     return (
         <div id="preview" dangerouslySetInnerHTML={{ __html: convertedMarkdown }} />
